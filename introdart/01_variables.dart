2481436ede484b1501a8952e93ac935139ce14cc
Eduar1505 <eduar.ventura@unah.hn>
2024-06-13 20:50:28 -0600

    variables

--- introdart/01_variables.dart
+++ introdart/01_variables.dart
@@ -1,6 +1,7 @@
 void main() {
-  String nombre = 'Juan';
+  String nombre = 'Eduar';
 //   dynamic apellido = 40; // permite la asignacion de cualquier valor y se puede volver a asignar y cambiar el tipo de dato
+  print(nombre);
 
   final String apellido;
 
@@ -11,8 +12,12 @@ void main() {
   //Null Safety
   String? comidaFavorita = null; // permite almancenar un string o un null
 
+  print(comidaFavorita);
+
   const String host =
       'https://unah.edu.hn'; // no se puede declarar sin asignar un valor (es inmutable)
+
+  print(host);
 
 //   nombre = 10; // no se puede cambiar el tipo de dato
   apellido = "Alvarenga";
@@ -20,6 +25,7 @@ void main() {
 //   apelido = ''; // ya no puede cambiar
 
   final int edad = 30;
+  print(edad);
 //   edad = 9;
-  bool esMayor;
+  //bool esMayor;
 }
